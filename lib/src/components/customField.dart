fc1252b302df82f9be6892e32f12a206a6b4d149
edmara <edmararocha007@gmail.com>
2022-09-12 08:56:11 -0300

    change styles of input component

--- lib/src/components/customField.dart
+++ lib/src/components/customField.dart
@@ -2,20 +2,27 @@ import 'package:flutter/material.dart';
 
 class CustomField extends StatelessWidget {
   final String labelText;
+  final TextInputType type;
 
-  const CustomField({super.key, required this.labelText});
+  const CustomField({super.key, required this.labelText, required this.type});
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      keyboardType: type,
       cursorColor: Colors.deepPurple,
       decoration: InputDecoration(
-          labelText: labelText,
-          labelStyle: const TextStyle(fontSize: 18, color: Colors.deepPurple),
-          enabledBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.deepPurple)),
-          focusedBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.deepPurple))),
+        labelText: labelText,
+        labelStyle: const TextStyle(fontSize: 16, color: Colors.deepPurple),
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.deepPurple,
+            ),
+            borderRadius: BorderRadius.circular(16)),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.deepPurple),
+            borderRadius: BorderRadius.circular(16)),
+      ),
     );
   }
 }
