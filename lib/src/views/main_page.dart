1802b4dbd06f5d65e9284759fecccce666d3ef70
Edmara Rocha <100307289+edmararocha@users.noreply.github.com>
2022-09-23 10:23:27 -0300

    Create, Read e Delete - CRUD

--- lib/src/views/main_page.dart
+++ lib/src/views/main_page.dart
@@ -1,7 +1,5 @@
-// ignore_for_file: unused_label
-
 import 'package:app_listagem/src/database/databaseHelper.dart';
-
+import 'package:app_listagem/src/views/update_page.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -35,7 +33,7 @@ class _MainPageState extends State<MainPage> {
         backgroundColor: Colors.deepPurple,
         actions: [
           ElevatedButton(
-            onPressed: () => {},
+            onPressed: () => {showAlertDialogInfo(context)},
             style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.deepPurple, elevation: 0),
             child: const Icon(Icons.info),
@@ -75,32 +73,44 @@ class _MainPageState extends State<MainPage> {
         itemBuilder: (BuildContext context, int index) {
           // ignore: prefer_const_constructors
           return GestureDetector(
-            onTap: () => {Navigator.pushReplacementNamed(context, '/add_page')},
-
-            child: Padding(
-            padding: const EdgeInsets.only(left: 5.0, right: 5.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  _patients[index]['_name'],
-                  style: const TextStyle(color: Colors.black54, fontSize: 20),
-                  textAlign: TextAlign.center,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    _deletar(_patients[index]['_id']);
+              onTap: () => {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => UpdatePage(
+                                id: _patients[index]['_id'],
+                                name: _patients[index]['_name'],
+                                email: _patients[index]['email'],
+                                gender: _patients[index]['gender'],
+                                about: _patients[index]['about'],
+                              )),
+                    )
                   },
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white60, elevation: 0),
-                  child: const Icon(
-                    Icons.delete,
-                    color: Colors.deepPurple,
-                  ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 5.0, right: 5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      _patients[index]['_name'],
+                      style:
+                          const TextStyle(color: Colors.black54, fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        _deletar(_patients[index]['_id']);
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white60, elevation: 0),
+                      child: const Icon(
+                        Icons.delete,
+                        color: Colors.deepPurple,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ));
+              ));
         },
         separatorBuilder: (BuildContext context, int index) => const Divider(),
       );
@@ -116,5 +126,31 @@ class _MainPageState extends State<MainPage> {
         });
       });
     });
+  }
+
+  showAlertDialogInfo(BuildContext context) {
+    // configura o button
+    Widget okButton = TextButton(
+      child: Text("OK"),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+    // configura o  AlertDialog
+    AlertDialog alerta = AlertDialog(
+      title: Text("Ajuda"),
+      content: Text(
+          '''Clique em um paciente para editá-lo ou pressione e segure para excluí-lo. '''),
+      actions: [
+        okButton,
+      ],
+    );
+    // exibe o dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alerta;
+      },
+    );
   }
 }
--- lib/src/views/update_page.dart
+++ lib/src/views/update_page.dart
@@ -1,15 +1,197 @@
 import 'package:flutter/material.dart';
 
+import '../components/customField.dart';
+import '../database/databaseHelper.dart';
+
+enum Gender { feminino, masculino }
+
 class UpdatePage extends StatefulWidget {
-  const UpdatePage({super.key});
+  const UpdatePage(
+      {super.key, this.id, this.name, this.email, this.gender, this.about});
+
+  final int? id;
+  final String? name;
+  final String? email;
+  final String? gender;
+  final String? about;
 
   @override
-  State<UpdatePage> createState() => _UpdatePageState();
+  State<UpdatePage> createState() =>
+    _UpdatePageState(id, name, email, gender, about);
 }
 
 class _UpdatePageState extends State<UpdatePage> {
+  Gender? _humangender = Gender.feminino;
+
+  final dbHelper = DatabaseHelper.instance;
+
+  final TextEditingController nameText = TextEditingController();
+  final TextEditingController emailText = TextEditingController();
+  final TextEditingController aboutText = TextEditingController();
+
+  final int? id;
+  final String? name;
+  final String? email;
+  String? gender;
+  final String? about;
+
+  _UpdatePageState(this.id, this.name, this.email, this.gender, this.about);
+
+  @override
+  void initState() {
+    _verifyGender();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: ElevatedButton(
+          onPressed: () => {Navigator.pushReplacementNamed(context, '/')},
+          style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.deepPurple, elevation: 0),
+          child: const Icon(Icons.arrow_back),
+        ),
+        title: const Text("Adicionar Paciente"),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(right: 25.0, left: 25.0, top: 50.0),
+          child: Column(
+            children: [
+              CustomField(
+                  labelText: 'Nome',
+                  type: TextInputType.name,
+                  controller: nameText),
+              const SizedBox(
+                height: 40,
+              ),
+              CustomField(
+                labelText: "Email",
+                type: TextInputType.emailAddress,
+                controller: emailText,
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Column(children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: const Text(
+                    "Sexo",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        color: Colors.deepPurple,
+                        fontSize: 18),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: Row(
+                        children: [
+                          Radio(
+                              activeColor: Colors.deepPurple,
+                              value: Gender.feminino,
+                              groupValue: _humangender,
+                              onChanged: (Gender? value) {
+                                setState(() {
+                                  _humangender = value;
+                                  gender = 'feminino';
+                                });
+                              }),
+                          const Expanded(
+                            child: Text('Feminino'),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      flex: 2,
+                      child: Row(
+                        children: [
+                          Radio(
+                              activeColor: Colors.deepPurple,
+                              value: Gender.masculino,
+                              groupValue: _humangender,
+                              onChanged: (Gender? value) {
+                                setState(() {
+                                  _humangender = value;
+                                  gender = 'masculino';
+                                });
+                              }),
+                          const Expanded(
+                            child: Text('Masculino'),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ]),
+              const SizedBox(
+                height: 50,
+              ),
+              CustomField(
+                labelText: "Sobre",
+                type: TextInputType.text,
+                controller: aboutText,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _updatePatient();
+                  Navigator.of(context).pushReplacementNamed('/');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                ),
+                child: const Text(
+                  "Adicionar",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _updatePatient() async {
+    Map<String, dynamic> row = {
+      DatabaseHelper.columnId: this.id,
+      DatabaseHelper.columnName: nameText.text,
+      DatabaseHelper.columnEmail: emailText.text,
+      DatabaseHelper.columnGender: gender,
+      DatabaseHelper.columnAbout: aboutText.text
+    };
+    final id = await dbHelper.update(row);
+    print('linha inserida id: $id');
+  }
+
+  _verifyGender() {
+    print("GENDER: $gender");
+    if (gender == 'feminino') {
+      _humangender = Gender.feminino;
+    } else {
+      _humangender = Gender.masculino;
+    }
   }
 }
